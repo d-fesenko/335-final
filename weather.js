97d9193f99a438fef50d614b3391e3b0191b6742
Areeb Malik <areebmalik2003@Areebs-MacBook-Pro.local>
2024-05-16 18:37:51 -0400

    added conditions

--- weather.js
+++ weather.js
@@ -14,37 +14,49 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+const apiKey = "298d89cce8ba1ad43545c91660409b12";
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=`;
 client.connect();
+
 app.get("/", (req, res) => res.render("index"));
 app.post("/weather", async (req, res) => {
-  const city = req.body.city;
-  const apiKey = "298d89cce8ba1ad43545c91660409b12"; 
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=`;
-
-  try {
+    const city = req.body.city;
     const response = await fetch(apiUrl + city.toLowerCase() + `&appid=${apiKey}`);
-    if (!response.ok) {
-      throw new Error("Failed to fetch weather data");
-    }
     const weatherData = await response.json();
-
-    const { temp, humidity } = weatherData.main;
-    const { speed } = weatherData.wind;
+    const temp = weatherData.main.temp;
+    const humidity = weatherData.main.humidity;
+    const { speed }= weatherData.wind;
+    const desc = weatherData.weather[0].description;
+    const descU = desc.split(" ").map(i => i[0].toUpperCase() + i.slice(1)).join(" ");
 
     const weather = {
       city: city,
       temperature: temp,
       humidity: humidity,
       wind: speed,
+      desc: descU,
     };
 
     await client.db("WeatherDB").collection("WeatherData").insertOne(weather);
 
     res.render("weather", { weather });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error fetching weather data");
-  }
+}); 
+
+app.post("/favorites", async (req, res) => {
+    const city = req.body.city;
+    await client.db("WeatherDB").collection("Favorites").insertOne({city});
+    res.redirect("/favorites");
+})
+app.get("/favorites", async (req, res) => {
+    const cities = await client.db("WeatherDB").collection("Favorites").find().toArray();
+    res.render("favorites", { cities });
+});
+
+app.post("/clear", async (req, res) => {
+    await client.db("WeatherDB").collection("Favorites").deleteMany({});
+    res.redirect("/favorites");
+  
 });
 
 app.listen(port, () => {
